59211c84f20603398cc9ddcd28a0b300d079fe16
김민석 <v0o0v@daum.net>
2022-03-23 12:58:51 +0900

    Initial commit

---
+++ src/main/java/com/example/archunittest/ArchunitTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.archunittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchunitTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchunitTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/archunittest/ArchunitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.archunittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchunitTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
